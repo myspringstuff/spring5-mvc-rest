34c77a0fed54703172a78ce473f3707b6a41a8f8
Jian-Min Gao <jian-min.gao@hpe.com>
2018-07-17 08:21:11 +0800

    swagger configuration

---
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package guru.springfamework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig{
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(metadata())
+                ;
+    }
+
+    private ApiInfo metadata() {
+        Contact contact = new Contact("David", "", "davidgjm@gmail.com");
+        return new ApiInfo(
+                "API doc demo",
+                "Customized swagger doc",
+                "1.0.demo",
+                "service url",
+                contact,
+                "MIT",
+                "license url",
+                Collections.emptyList()
+        );
+    }
+
+}
